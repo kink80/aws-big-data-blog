349aaef9b284fc2cac6760509688aa727916fa04
Slavek Tecl <slavomir.tecl@blackboard.com>
2015-10-28 11:54:25 +0100

    Moved parsing to a separate thread

--- extract-common-crawl-urls/commoncrawl.cascading.extracturls/src/main/java/com/amazonaws/bigdatablog/indexcommoncrawl/parser/TikaParser.java
+++ extract-common-crawl-urls/commoncrawl.cascading.extracturls/src/main/java/com/amazonaws/bigdatablog/indexcommoncrawl/parser/TikaParser.java
@@ -1,17 +1,43 @@
 package com.amazonaws.bigdatablog.indexcommoncrawl.parser;
 
-import java.io.IOException;
 import java.io.InputStream;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
+import org.apache.log4j.Logger;
 import org.apache.tika.Tika;
-import org.apache.tika.exception.TikaException;
+
 
 public class TikaParser {
+
+	private static Logger LOGGER = Logger.getLogger(TikaParser.class);
 	
 	private static final Tika tika = new Tika();
+
+	private static final ExecutorService executor = Executors.newSingleThreadExecutor();
 	
-	public static String parse(InputStream is) throws IOException, TikaException {
-		return tika.parseToString(is);
+	public static String parse(final InputStream is)  {
+		Callable<String> task = new Callable<String>() {
+
+			@Override
+			public String call() throws Exception {
+				return tika.parseToString(is);
+			}
+
+		};
+
+		Future<String> result = executor.submit(task);
+
+		String extractedText = "";
+		try {
+			extractedText = result.get();
+		} catch (Exception ee) {
+			LOGGER.error("Thread interrupted while extracting the text.", ee);
+		}
+
+		return extractedText;
 	}
 
 }
